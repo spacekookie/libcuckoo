d905d74bbd61b632188d2d9339af40e7a23a275f
Katharina Fey <kookie@spacekookie.de>
2017-09-01 00:42:23 +0200

    Resizing tables when insert fails

--- src/insert.c
+++ src/insert.c
@@ -41,9 +41,9 @@ int cuckoo_insert(struct cuckoo_map *map, const char *key, void *data)
     memset(item, 0, sizeof(cc_map_item));
 
     /** Create a new cc_map_item with all fields set */
-    item->prev = (short) TABLE_INIT;
-    item->table = TABLE_INIT;
-    item->qu_idx = IGNORE;
+    item->prev = (uint8_t) TABLE_INIT;
+    item->table = (uint8_t) TABLE_INIT;
+    item->qu_idx = (uint8_t) IGNORE;
     item->key = malloc(sizeof(char) * strlen(key) + 1);
     strcpy(item->key, key);
     item->value = data;
@@ -59,6 +59,27 @@ int cuckoo_insert(struct cuckoo_map *map, const char *key, void *data)
 
         rec_ctr++;
         if(rec_ctr >= map->rec_lim) {
+
+            /* Increase table size when we fail to insert */
+            int i;
+            size_t size = map->size * 2;
+            size_t memsize = sizeof(cc_map_item) * size;
+
+            cc_map_item ***tmp = (cc_map_item***) calloc(sizeof(cc_map_item), map->num_tables);
+            for(i = 0; i < map->num_tables; i++) {
+                tmp[i] = malloc(memsize);
+                if(tmp[i] == NULL) {
+                    ret = CUCKOO_MALLOC_FAIL;
+                    break;
+                }
+
+                memcpy(tmp[i], map->tables[i], map->size);
+                free(map->tables[i]);
+                map->tables[i] = tmp[i];
+            }
+
+            free(tmp);
+            map->size = size;
             ret = CUCKOO_FAILED_INSERT;
             break;
         }
